01c90570ff14315ea7c0fcff9c0e88f8596df7a7
Pawel Wieladek <pawelwieladek@gmail.com>
2014-11-07 19:40:54 +0100

    Structure done

--- app/server/server.js
+++ app/server/server.js
@@ -2,18 +2,20 @@ var express = require('express');
 var app = express();
 var path = require("path");
 
-var RegressionProblem = require("../problems/regression");
-
+// API
+var RegressionProblem = require("../../lib/src/problems/regression");
 app.get('/regression', function (req, res) {
     var regression = new RegressionProblem();
-    regression.solve("../resources/regression.train.csv")
+    regression.solve()
         .then(function(result) {
             res.send(result);
         });
 });
 
-app.use(express.static(path.join(__dirname, "../app/dist")));
+// Static files
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
+// Run server
 var server = app.listen(3000, function () {
 
     var host = server.address().address;
--- gulpfile.js
+++ gulpfile.js
@@ -5,37 +5,50 @@ var browserify = require('browserify');
 var del = require('del');
 var source = require("vinyl-source-stream");
 
-gulp.task('test', function () {
-    return gulp.src('**/*.test.js')
+var paths = {
+    app: {
+        client: {
+            src: "./app/client/src",
+            dist: "./app/client/dist"
+        },
+        server: "./app/server"
+    },
+    lib: {
+        src: "./lib/src"
+    }
+};
+
+gulp.task("test", function () {
+    return gulp.src(paths.lib.src + "/*.test.js")
         .pipe(jasmine());
 });
 
-gulp.task('clean', function (cb) {
-    del(['./src/app/dist'], cb);
+gulp.task("clean", function (cb) {
+    del([paths.app.client.dist], cb);
 });
 
-gulp.task('html', function () {
-    return gulp.src('./src/app/src/*.html')
-        .pipe(gulp.dest('./src/app/dist'));
+gulp.task("html", function () {
+    return gulp.src(paths.app.client.src + "/*.html")
+        .pipe(gulp.dest(paths.app.client.dist));
 });
 
-gulp.task('scripts', function () {
-    return browserify('./src/app/src/app.js')
+gulp.task("scripts", function () {
+    return browserify(paths.app.client.src + "/app.js")
         .bundle()
-        .pipe(source('app.js'))
-        .pipe(gulp.dest('./src/app/dist'))
+        .pipe(source("app.js"))
+        .pipe(gulp.dest(paths.app.client.dist))
 });
 
-gulp.task('build', ['html', 'scripts']);
+gulp.task("build", ["html", "scripts"]);
 
-gulp.task('serve', ['build'], function () {
+gulp.task("serve", ["build"], function () {
     //start the server at the beginning of the task
     server.run({
-        file: './src/server/server.js'
+        file: paths.app.server + "/server.js"
     });
 
     //restart the server when file changes
-    gulp.watch(['**/*.html'], [server.notify]);
-    gulp.watch(['./src/server/server.js'], [server.run]);
-    gulp.watch(['./src/app/src/**/*.js'], ['scripts', server.run]);
+    gulp.watch([paths.app.client.src + "**/*.html"], ["build", server.run]);
+    gulp.watch([paths.app.client.src + "/**/*.js"], ["build", server.run]);
+    gulp.watch([paths.app.server + "/server.js"], [server.run]);
 });
